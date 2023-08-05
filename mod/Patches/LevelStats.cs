cea26134348d6a0de98936a86291ffed17333a38
trevorleslie <80716066+TRPG0@users.noreply.github.com>
2023-08-04 23:57:54 -0600

    Open stats screen on first time playing level

---
+++ mod/Patches/LevelStats.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace ArchipelagoULTRAKILL.Patches
+{
+    [HarmonyPatch(typeof(LevelStats), "Start")]
+    public class LevelStats_Start_Patch
+    {
+        public static void Postfix(LevelStats __instance)
+        {
+            if (Core.DataExists())
+            {
+                if (StockMapInfo.Instance != null) __instance.levelName.text = StockMapInfo.Instance.assets.LargeText;
+                Traverse.Create(__instance).Field<bool>("ready").Value = true;
+            }
+        }
+    }
+}
---
+++ mod/Patches/LevelStatsEnabler.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace ArchipelagoULTRAKILL.Patches
+{
+    [HarmonyPatch(typeof(LevelStatsEnabler), "Start")]
+    public class LevelStatsEnabler_Start_Patch
+    {
+        public static void Postfix(LevelStatsEnabler __instance)
+        {
+            if (Core.DataExists())
+            {
+                __instance.gameObject.SetActive(true);
+            }
+        }
+    }
+}
